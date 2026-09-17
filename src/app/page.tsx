80c7341ecdd737a779ebef9847375c83b325fec3
Kyle Blair <github.freeware771@passmail.net>
2026-09-16 17:07:47 -0700

    feat: rewrite the landing page blurb yet again
    Communicating via writing is surprisingly complex and challenging.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,12 @@ export default function Home() {
 					<p className="eyebrow">home</p>
 					<h1>kyle blair</h1>
 					<p className="hero__statement">
-						I like creating surprisingly versatile solutions by avoiding
-						unnecessary assumptions and applying a little thoughtful design. I
-						have a computer engineering degree, a decade of experience as a
-						software engineer, and a homelab that always needs a new feature.
+						I like creating surprisingly capable solutions by questioning
+						assumptions, understanding what actually drives the problem, and
+						applying thoughtful design where it matters most. I combine that
+						mindset with a computer engineering degree and a decade of software
+						engineering experience, using it throughout life, from professional
+						projects to my homelab that always needs a new feature.
 					</p>
 				</div>
 			</section>
